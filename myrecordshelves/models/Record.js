ae59704175a0d13c2c404094f816d787207ead9a
David Thorpe <davidthorpe878@gmail.com>
2017-09-19 22:19:14 +0100

    Updated starting files and added initial Record Schema

---
+++ myrecordshelves/models/Record.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const recordSchema = new mongoose.Schema({
+	artist: {
+		type: String,
+		trim: true,
+		required: 'Please enter the artist\'s name!'
+	},
+	title: {
+		type: String,
+		trim: true,
+		required: 'Please enter the record\'s name!'
+	},
+	shelf: {
+		type: Number
+	},
+	slug: String,
+	description: {
+		type: String,
+		trim: true
+	},
+	tags: [String]
+});
+
+recordSchema.pre('save', function(next) {
+	if (!this.isModified('title')) {
+		next(); //skip it
+		return; // stops this function from running
+	}
+	this.slug = slug(this.title);
+	next();
+	// TODO: update this to make more resillient so slugs are unique for matching titles
+});
+
+module.exports = mongoose.model('Record', recordSchema);
