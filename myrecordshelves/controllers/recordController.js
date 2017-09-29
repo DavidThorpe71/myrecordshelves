082d42da12e4fb211bedb773dc7680587523dc5c
David Thorpe <davidthorpe878@gmail.com>
2017-09-29 20:25:21 +0100

    Implement save record to database

--- myrecordshelves/controllers/recordController.js
+++ myrecordshelves/controllers/recordController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Record = mongoose.model('Record');
+
 exports.homePage = (req, res) => {
 	res.render('index', { title: 'My Record Shelves'} );
 };
@@ -5,3 +8,9 @@ exports.homePage = (req, res) => {
 exports.addRecord = (req, res) => {
 	res.render('editRecord', { title: 'Add Record' });
 };
+
+exports.createRecord = async (req, res) => {
+	const record = new Record(req.body);
+	await record.save();
+	res.redirect('/');
+}
--- myrecordshelves/routes/index.js
+++ myrecordshelves/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const recordController = require('../controllers/recordController')
+const recordController = require('../controllers/recordController');
+const { catchErrors } =require('../handlers/errorHandlers');
 
 /* GET home page. */
 router.get('/', recordController.homePage);
 router.get('/add', recordController.addRecord);
+router.post('/add', catchErrors(recordController.createRecord));
 
 module.exports = router;
